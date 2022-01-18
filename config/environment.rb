4a34b330b71e72ab2dbe411441991482e2da9251
Ronniel de Ramos <ronnielderamos@gmail.com>
2022-01-18 10:40:44 +0800

    production

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,12 @@ require_relative "application"
 Rails.application.initialize!
 
 
-ActionMailer::Base.smtp_settings = {
-  address: 'smtp.sendgrid.net',
-  port: '587',
-  authentication: :plain,
-  user_name: 'apikey',
-  password: Rails.application.credentials.sendgrid[:apikey],
-  domain: 'instacram-beta.herokuapp.com',
-  enable_starttls_auto: true
-}
+# ActionMailer::Base.smtp_settings = {
+#   address: 'smtp.sendgrid.net',
+#   port: '587',
+#   authentication: :plain,
+#   user_name: 'apikey',
+#   password: Rails.application.credentials.sendgrid[:apikey],
+#   domain: 'instacram-beta.herokuapp.com',
+#   enable_starttls_auto: true
+# }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,17 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
   config.action_mailer.perform_deliveries = true
 
+  config.action_mailer.default_url_options = {host: "instacram-beta.herokuapp.com", protocol: "https"}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            Rails.application.credentials.dig(:gmail, :email),
+    password:             Rails.application.credentials.dig(:gmail, :password),
+    authentication:       'plain',
+    enable_starttls_auto: true}
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
@@ -66,7 +77,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
