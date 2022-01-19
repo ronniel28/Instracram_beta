edee3b6499dde465d3d3bffd5dc46b6f6a773b1c
Ronniel de Ramos <ronnielderamos@gmail.com>
2022-01-19 23:45:13 +0800

    test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,9 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
-    @profile = Profile.find(current_user.profile.id)
+    if current_user
+      @profile = Profile.find(current_user.profile.id)
+    end
     @images = Image.where(user_id: current_user)
   end
 end
