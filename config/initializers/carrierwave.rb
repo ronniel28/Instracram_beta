7732faff1383fbdf9ae37e7b6301cb2f5779a991
Ronniel de Ramos <ronnielderamos@gmail.com>
2022-01-19 16:07:14 +0800

    test

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,19 @@
-if Rails.env.production?
-    CarrierWave.configure do |config|
-        config.fog_credentials = {
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+CarrierWave.configure do |config|
+  if Rails.env.production?
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_public = false
+    config.fog_directory = "instacram2"
+    config.fog_credentials = {
             provider: 'AWS',
             aws_access_key_id: Rails.application.credentials.dig(:aws, :access_key_id),
             aws_secret_access_key: Rails.application.credentials.dig(:aws, :secret_access_key),
-            region: 'ap-southeast-1'
+            region: 'ap-southeast-1',
+            path_style: true
         }
-        config.storage = :fog
-        config.fog_directory = "instacram2"
+        
     end
 end
