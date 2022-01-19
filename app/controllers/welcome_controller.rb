3c198dad54a21121de56dd54541e7026c3c256ac
Ronniel de Ramos <ronnielderamos@gmail.com>
2022-01-19 23:12:16 +0800

    edit profile photo

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
   def index
+    @profile = Profile.find(current_user.profile.id)
     @images = Image.where(user_id: current_user)
   end
 end
